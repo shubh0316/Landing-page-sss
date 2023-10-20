a67bcbec3bef6b3202ef77c7d7cc43367db8522e
shubhanker <ankursaxena347@gmail.com>
2023-10-20 14:41:34 +0530

    deploy

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -9,10 +9,10 @@ const About = () => {
        <main className="flex flex-col items-center justify-center self-center p-2.5">
          <AnimatedText text={"Passion Fuels Purpose!"} className="text-8xl text-center" />
          <div className="uppercase text-lg font-semibold">
-         BIOGRAPHY
+          biography
          </div>
          <div className="text-lg text-center py-4">
-         I am a passionate computer science graduate with a B.Tech degree, specializing in web development and software development from the ground up. With a strong foundation in various programming languages and frameworks, I bring a wealth of experience to the table. I excel in crafting stunning landing pages infused with captivating animations, creating immersive user experiences that leave a lasting impression. My versatility extends across the tech stack, allowing me to adapt seamlessly to any projects requirements. I am committed to delivering innovative and high-quality solutions, and I look forward to contributing my skills to your next web or software development endeavor.
+         I am a passionate computer science graduate with a Bachelors in Technology degree, specializing in web development and software development from the ground up. With a strong foundation in various programming languages and frameworks, I bring a wealth of experience to the table. I excel in crafting stunning landing pages infused with captivating animations, creating immersive user experiences that leave a lasting impression. My versatility extends across the tech stack, allowing me to adapt seamlessly to any projects requirements. I am committed to delivering innovative and high-quality solutions, and I look forward to contributing my skills to your next web or software development endeavor.
          </div>
        </main>
        <Skills />
